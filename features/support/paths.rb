78b50d9a0e84ee2f71f9031f1df0e997949e2f1f
salem <salemamba@gmail.com>
2015-01-19 09:59:22 -0800

    initial commit

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -18,6 +18,9 @@ module NavigationHelpers
     when /^the new article page$/
       '/admin/content/new'
 
+    when /^the login page$/
+      '/accounts/login'
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
